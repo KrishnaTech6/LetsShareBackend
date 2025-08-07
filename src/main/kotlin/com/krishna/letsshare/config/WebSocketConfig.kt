4138070731ca037c8cecb06155aba8749d71e168
krishnaTech6 <krishna060702@gmail.com>
2025-08-07 23:58:59 +0530

    increases buffer size

--- src/main/kotlin/com/krishna/letsshare/config/WebSocketConfig.kt
+++ src/main/kotlin/com/krishna/letsshare/config/WebSocketConfig.kt
@@ -1,10 +1,13 @@
 package com.krishna.letsshare.config
 
 import com.krishna.letsshare.handler.ScreenShareHandler
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.socket.config.annotation.EnableWebSocket
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler
 
 @Configuration
 @EnableWebSocket
@@ -12,5 +15,15 @@ class WebSocketConfig : WebSocketConfigurer {
     override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
         registry.addHandler(ScreenShareHandler(), "/ws/screen")
             .setAllowedOrigins("*")
+            .setHandshakeHandler(DefaultHandshakeHandler())
+    }
+
+    @Bean
+    fun createWebSocketContainer(): ServletServerContainerFactoryBean {
+        val container = ServletServerContainerFactoryBean()
+        container.setMaxTextMessageBufferSize(512 * 1024)  // 512 KB
+        container.setMaxBinaryMessageBufferSize(512 * 1024) // 512 KB
+        container.setAsyncSendTimeout(10_000L) // 10 seconds
+        return container
     }
 }
